fc2ae1f40ce6764dcd517944fb0dde443132d6b6
Janicek2 <bjanicki82@gmail.com>
2018-06-04 11:18:59 +0200

    Add colors

---
+++ modules/OSInfo.js
@@ -0,0 +1,25 @@
+process.stdin.setEncoding('utf-8');
+var time = require('../modules/time');
+var os = require('os');
+var colors = require('colors');
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:'.grey, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: '.green, time.print(uptime));
+    console.log('User name:'.yellow.underline, userInfo.username);
+    console.log('Home dir:'.rainbow, userInfo.homedir);
+  }
+
+  exports.print = getOSinfo;
