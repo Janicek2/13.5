d907e3433699a0ab6225de4bc7ca2a732667ad9d
Bartosz Janicki <janicek2@gmail.com>
2018-06-13 10:07:39 +0200

    Http

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
-var EventEmitter = require("events").EventEmitter;
-var OSinfo = require('./modules/OSinfo');
+var http = require('http');
+var fs = require('fs');
 
-var emitter = new EventEmitter();
-emitter.on("beforeCommand", function (instruction) {
-    console.log('Napisałeś: ' + instruction + ', aby uruchomić komendę');
-});
-emitter.on("afterCommand", function () {
-    console.log('Koniec polecenia');
-});
-process.stdin.setEncoding('utf-8');
+var server = http.createServer();
 
-process.stdin.on('readable', function() {
-  var input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(input);
-    var instruction = input.toString().trim();
-    emitter.emit('beforeCommand', instruction);
-    switch (instruction) {
-      case 'version':
-        process.stdout.write('Node version: ' + process.versions.node + '\n');
-        break;
-      case 'language':
-        process.stdout.write('Language is: ' + process.env.LANG + '\n');
-        break;
-      case '/exit':
-        process.stdout.write('Wychodzimy!\n');
-        process.exit();
-        break;
-      case '/info':
-        OSinfo.print();
-        break;
-      default:
-        process.stderr.write('Złe instrukcje!\n');
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/hello') {
+        response.write('<h1>Hello World!</h1>');
+            response.end();
+    } else {
+            response.statusCode = 404;
+            response.write('<h1>404: Zła ścieżka!</h1>');
+            response.end();
     }
-    emitter.emit('afterCommand');
-  }
 });
+
+server.listen(8080);
